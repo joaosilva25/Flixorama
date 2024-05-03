3355ac28a51e6586c1debc9aab0f0b1b28a32611
JoaoSilva25 <joaosilva2505@outlook.com>
2024-05-03 19:20:13 -0300

    delete tabindex

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -131,9 +131,9 @@ export const MenuBar=()=> {
                             </button>
                         </div>
                         {visibleMenu && (
-                            <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                            <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
                                 <div className="py-1" role="none">
-                                    <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Minha Conta</a>
+                                    <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-0">Minha Conta</a>
                                     <button className='text-black relative left-4' onClick={(e)=>{e.preventDefault();logoutUser()}}>Sair</button>
                                 </div>
                             </div>
